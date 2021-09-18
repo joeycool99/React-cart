77f937244e4fb8508091d58f844f1f50640f2dbe
joeycool99 <naveenjoe@gmail.com>
2021-09-18 10:20:02 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,318 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from "react"
-import React from 'react';
+import "./index.css";
+import { useState } from "react";
 
-function App(props) {
+function App() {
+  const [arr] = useState([
+    {
+      item: "Special Item",
+      orginalprice: "$20.00 ",
+      discountprice: "$18.00",
+      img: "https://images.indianexpress.com/2017/11/cosmetics-759.jpg"
+    },
+    {
+      item: "Fancy Product",
+      orginalprice: "$40.00 ",
+      discountprice: "$20.00",
+      img:
+        "https://www.webfx.com/blog/wp-content/uploads/2021/04/jcp-product-categories.png"
+    },
+    {
+      item: "Sale Item",
+      orginalprice: "$50.00 ",
+      discountprice: "$25.00",
+      img: "https://skeetjecosmetics.files.wordpress.com/2014/03/3096.jpg"
+    },
+    {
+      item: "Special Item",
+      orginalprice: "$20.00 ",
+      discountprice: "$18.00",
+      img:
+        "https://image.shutterstock.com/image-vector/glasses-line-art-silhouette-eyewear-260nw-1697413246.jpg"
+    },
+    {
+      item: "Popular Item",
+      orginalprice: "$32.00 ",
+      discountprice: "$22.00",
+      img: "https://m.media-amazon.com/images/I/61pbYlXndgL._UX569_.jpg"
+    },
+    {
+      item: "Fancy Product",
+      orginalprice: "$280.00 ",
+      discountprice: "$120.00",
+      img:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gh-best-selling-amazon-products-1586979875.png"
+    },
+    {
+      item: "Toys",
+      orginalprice: "$20.00 ",
+      discountprice: "$18.00",
+      img:
+        "https://upload.wikimedia.org/wikipedia/commons/4/4e/Wooden_toys_%28cropped%29.JPG"
+    },
+    {
+      item: " Rare collections",
+      orginalprice: "$140.00 ",
+      discountprice: "$90.00",
+      img: "https://www.junglescout.com/wp-content/uploads/2020/06/cathero.png"
+    }
+  ]);
 
-  return (
+  var [cart, setcart] = useState([]);
+  var [page, setpage] = useState("products");
 
-
-
-    <div class="col-lg-4">
-      <div class="card mb-5 mb-lg-0">
-        <div class="card-body">
-          <h5 class="card-title text-muted text-uppercase text-center">{props.free}</h5>
-          <h6 class="card-price text-center">${props.price}<span class="period">/month</span></h6>
-          <hr></hr>
-          <ul class="fa-ul">
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>{props.User}</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>{props.Storage}</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>{props.Projects}</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>{props.Access}</li>
-            <li class={props.muted}><span class="fa-li">{props.icon1}</span>{props.Private}</li>
-            <li class={props.muted}><span class="fa-li">{props.icon2}</span>{props.Phone}
-            </li>
-            <li class={props.muted}><span class="fa-li">{props.icon3}</span>{props.Subdomain}
-            </li>
-            <li class={props.muted1}><span class="fa-li">{props.icon4}</span>{props.Reports}</li>
-          </ul>
-          <div class="d-grid">
-            <a href="#" class="btn btn-primary text-uppercase">Button</a>
+  function cardadd(products) {
+    setcart([...cart, { ...products }]);
+  }
+  function navigateto(newpage) {
+    setpage(newpage);
+  }
+  function removefromcart(producttoremove) {
+    setcart(cart.filter((item) => item !== producttoremove));
+  }
+  const products = () => (
+    <div>
+      <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+          <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Shop in style</h1>
           </div>
         </div>
-      </div>
-    </div>
+      </header>
 
+      <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+          <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            {arr.map((data, i) => (
+              <div class="col mb-5">
+                <div class="card h-100">
+                  <div
+                    class="badge bg-dark text-white position-absolute"
+                    style={{ top: "0.5rem", right: "0.5rem" }}
+                  >
+                    Sale
+                  </div>
+
+                  <img class="card-img-top" src={data.img} alt="..." />
+
+                  <div class="card-body p-4">
+                    <div class="text-center">
+                      <h5 class="fw-bolder">{data.item}</h5>
+                      <div class="d-flex justify-content-center small text-warning mb-2">
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                      </div>
+                      <span class="text-muted text-decoration-line-through">
+                        {data.orginalprice}
+                      </span>
+                      {data.discountprice}
+                    </div>
+                  </div>
+
+                  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center">
+                      <div>
+                        <button
+                          onClick={() => cardadd(data)}
+                          class="btn btn-outline-dark mt-auto"
+                          href="#"
+                        >
+                          add to cart
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <footer class="py-5 bg-dark">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; Your Website 2021
+          </p>
+        </div>
+      </footer>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+
+      <script src="js/scripts.js"></script>
+    </div>
+  );
+  const rendercart = () => (
+    <div>
+      <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+          <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Cart</h1>
+            <p class="lead fw-normal text-white-50 mb-0"></p>
+          </div>
+        </div>
+      </header>
+
+      <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+          <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            {cart.map((data, i) => (
+              <div class="col mb-5">
+                <div class="card h-100">
+                  <div
+                    class="badge bg-dark text-white position-absolute"
+                    style={{ top: "0.5rem", right: "0.5rem" }}
+                  >
+                    Sale
+                  </div>
+
+                  <img class="card-img-top" src={data.img} alt="..." />
+
+                  <div class="card-body p-4">
+                    <div class="text-center">
+                      <h5 class="fw-bolder">{data.item}</h5>
+                      <div class="d-flex justify-content-center small text-warning mb-2">
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                        <div class="bi-star-fill"></div>
+                      </div>
+                      <span class="text-muted text-decoration-line-through">
+                        {data.orginalprice}
+                      </span>
+                      {data.discountprice}
+                    </div>
+                  </div>
+
+                  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div class="text-center">
+                      <div>
+                        <button
+                          onClick={() => removefromcart(data)}
+                          class="btn btn-outline-dark mt-auto"
+                          href="#"
+                        >
+                          remove from cart
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <footer class="py-5 bg-dark">
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright &copy; Your Website 2021
+          </p>
+        </div>
+      </footer>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+
+      <script src="js/scripts.js"></script>
+    </div>
+  );
+
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container px-4 px-lg-5">
+          <a class="navbar-brand" href="#!">
+            Cosmetics
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#!">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#!">
+                  About
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  id="navbarDropdown"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Shop
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a class="dropdown-item" href="#!">
+                      All Products
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#!">
+                      Popular Items
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#!">
+                      New Arrivals
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <button
+                onClick={() => navigateto("cart")}
+                class="btn btn-outline-dark"
+                type="submit"
+              >
+                <i class="bi-cart-fill me-1"></i>
+                Go to Cart
+                <span class="badge bg-dark text-white ms-1 rounded-pill">
+                  {cart.length}
+                </span>
+              </button>
+              <button
+                style={{ marginLeft: "6px" }}
+                onClick={() => navigateto("products")}
+                class="btn btn-outline-dark"
+                type="submit"
+              >
+                <i class="bi-cart-fill me-1"></i>
+                Products
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      {page === "products" && products()}
+      {page === "cart" && rendercart()}
+    </div>
   );
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,98 +1,13 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
 
-var arr = [
-  {
-    price: "0",
-    free: "free",
-    User: "Single User",
-    muted: "text-muted",
-    muted1: "text-muted",
-    Storage: "5GB Storage",
-    Projects: "Unlimited Public Projects",
-    Access: "Community Access",
-    Private: "Unlimited Private Projects",
-    Phone: " Dedicated Phone Support",
-    Subdomain: "Free Subdomain",
-    Reports: "Monthly Status Reports",
-    icon1: [<i class="fas fa-times"></i>],
-    icon2: [<i class="fas fa-times"></i>],
-    icon3: [<i class="fas fa-times"></i>],
-    icon4: [<i class="fas fa-times"></i>]
-  },
-  {
-    price: "9",
-    free: "PLUS",
-    User: [<strong>5 Users</strong>],
-    muted1: "text-muted",
-    Storage: "50GB Storage",
-    Projects: "Unlimited Public Projects",
-    Access: "Community Access",
-    Private: "Unlimited Private Projects",
-    Phone: " Dedicated Phone Support",
-    Subdomain: "Free Subdomain",
-    Reports: "Monthly Status Reports",
-    icon4: [<i class="fas fa-times"></i>],
-    icon1: [<i class="fas fa-check"></i>],
-    icon2: [<i class="fas fa-check"></i>],
-    icon3: [<i class="fas fa-check"></i>]
-  },
-  {
-    price: "49",
-    free: "PRO",
-    User: [<strong>Unlimited Users</strong>],
-    Storage: "150GB Storage",
-    Projects: "Unlimited Public Projects",
-    Access: "Community Access",
-    Private: "Unlimited Private Projects",
-    Phone: " Dedicated Phone Support",
-    Subdomain: [<strong>Unlimited</strong>, " Free Subdomains"],
-    Reports: "Monthly Status Reports",
-    icon4: [<i class="fas fa-check"></i>],
-    icon1: [<i class="fas fa-check"></i>],
-    icon2: [<i class="fas fa-check"></i>],
-    icon3: [<i class="fas fa-check"></i>]
-  }
-];
-
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <div>
-    <section class="pricing py-5">
-      <div class="container">
-        <div class="row">
-          {arr.map((plan) => (
-            <App
-              price={plan.price}
-              free={plan.free}
-              User={plan.User}
-              muted={plan.muted}
-              muted1={plan.muted1}
-              Storage={plan.Storage}
-              Projects={plan.Projects}
-              Access={plan.Access}
-              Private={plan.Private}
-              Phone={plan.Phone}
-              Subdomain={plan.Subdomain}
-              Reports={plan.Reports}
-              icon1={plan.icon1}
-              icon2={plan.icon2}
-              icon3={plan.icon3}
-              icon4={plan.icon4}
-            />
-          ))}
-        </div>
-      </div>
-    </section>
-  </div>,
-  document.getElementById("root")
+  <StrictMode>
+    {/* cutomise the component here */}
+    <App />
+  </StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
